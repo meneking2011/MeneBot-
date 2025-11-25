533a6b4186dde3f7361e2b1a7a521a4a62d795a5
meneking2011 <emmanueloni242011@gmail.com>
2025-11-25 01:55:23 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Use modern ReactDOM client
-import App from "./App.jsx";
+import App from "../App.jsx";
 
 // Standard entry point to render the App component into the HTML DOM.
 const root = ReactDOM.createRoot(document.getElementById("root"));
